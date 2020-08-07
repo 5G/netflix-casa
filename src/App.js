400de99f036b97bd3054e1b2acc042d24a5a44b7
James <james@giveback.ie>
2020-08-07 11:58:41 +0100

    Update App.js
    Testing Heroku Deployment Pipeline

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <Nav />
       <Banner />
       <Row
-        title="NETFLIX ORIGINALS"
+        title="NETFLIX ORIGINALS TEST"
         fetchUrl={requests.fetchNetflixOriginals}
         isLargeRow
       />
